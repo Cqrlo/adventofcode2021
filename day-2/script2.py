d27f5e0f79771485ec2e4f9d17a25c94962d0c68
Cqrlo <112483@emmauscollege.nl>
2022-10-09 21:43:38 +0000

    Day 2 klaar :))

--- day-2/script2.py
+++ day-2/script2.py
@@ -4,15 +4,17 @@ with open('input.txt') as file:
 
 horizontal = 0
 depth = 0
+aim = 0
 currentLineSplit = [" ", " "]
 
 for line in lines:
     currentLineSplit = line.split()
     if (currentLineSplit[0] == "forward"):
         horizontal += int(currentLineSplit[1])
-    if (currentLineSplit[0] == "up"):
-        depth -= int(currentLineSplit[1])
-    if (currentLineSplit[0] == "down"):
-        depth += int(currentLineSplit[1])
+        depth      += int(currentLineSplit[1]) * aim
+    elif (currentLineSplit[0] == "up"):
+        aim        -= int(currentLineSplit[1])
+    elif (currentLineSplit[0] == "down"):
+        aim        += int(currentLineSplit[1])
 
 print(horizontal * depth)
